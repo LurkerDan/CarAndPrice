90fb61c591284150a943c542e6dbab7ecd7c5adf
LurkerDan <89412351+LurkerDan@users.noreply.github.com>
2022-02-24 20:42:26 +0600

    Add files via upload

--- Analyzer.py
+++ Analyzer.py
@@ -3,17 +3,19 @@ import re
 
 class Analyzer:
 
+    # Удаление знаков HTML разметки из массива
     def delete_sings(self, arr):
         self.arr = arr
         clear_arr = []
 
         for i in range(len(arr)):
-            move = dict.fromkeys((ord(c) for c in u" \xa0\n\t"))
+            move = dict.fromkeys((ord(c) for c in u" \xa0\n\t\₸"))
             temp = (arr[i].translate(move))
             clear_arr.append(temp)
 
         return clear_arr
 
+    # Удаление лишних пробелов из массива
     def delete_space(self, arr):
         self.arr = arr
         clear_arr = []
@@ -28,6 +30,7 @@ class Analyzer:
             clear_arr.append(clear_str)
         return clear_arr
 
+    # Поиск целочисленных значений из строки
     def find_int(self, string):
         str_pages = re.findall(r'\d+', string)
         int_pages = int(str_pages[0] + str_pages[1])
--- main.py
+++ main.py
@@ -8,31 +8,41 @@ gsheet = GoogleSheet()
 
 page = 0
 
+# Обращение странице и поиск общего кол-ва объявлений деленное на колличество объявлений дает общее количество страниц
+response = requests.get('https://kolesa.kz/cars/?page=' + str(page))
+html = BeautifulSoup(response.content, 'html.parser')
+pages = str(html.find_all('div', class_='finded'))
+
+total_pages = analyzer.find_int(pages)
+print(f'Всего страниц на сайте {total_pages} \n Всего объявлений на сайте {total_pages *20}')
 arr_cars = []
 arr_prices = []
-while page!=3:
+
+# Суммарное колличество обработки страниц
+while page != 1:
     page += 1
 
+    # Обращение к HTML странице
     response = requests.get('https://kolesa.kz/cars/?page=' + str(page))
     html = BeautifulSoup(response.content, 'html.parser')
 
+    # Поиск Raw цен и машин на странице
     prices = html.find_all('span', class_='price')
     cars = html.select('div > div.a-info-top')
 
-    pages = str(html.find_all('div', class_='finded'))
-    total_pages = analyzer.find_int(pages)
-
+    # Цикл обработки массива машин с одной страницы
     for i in cars:
         car = i.select('span.a-el-info-title > a')
         arr_cars.append(car[0].text)
         total_cars = analyzer.delete_space(arr_cars)
 
+    # Цикл обработки массива цен с одной страницы
     for i in range(20):
         arr_prices.append(prices[i].text)
         total_prices = analyzer.delete_sings(arr_prices)
 
-print(total_prices)
-print(total_cars)
+# Упаковка массивов для отправки
 total = list(zip(total_prices,total_cars))
 
-print(total)
+# Отправка в гугл таблицы готового массива
+gsheet.google_request(total)
